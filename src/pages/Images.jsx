25a2b9f09d060afaeb12510a79665a4deb7e4e98
alex-serb13 <alex.serb@globant.com>
2022-07-21 10:34:20 +0300

    implement Images page with images from storage

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -1,3 +1,43 @@
+import { List, Image, Card } from "antd";
+import { EmptyPage } from "../components/EmptyPage";
+
 export const Images = () => {
-  return <h1>This is the Images page</h1>;
+  const storedImages = localStorage.getItem("images") || JSON.stringify([]);
+  const images = JSON.parse(storedImages);
+
+  if (!images.length) {
+    return <EmptyPage description={<span>No Images Uploaded Yet</span>} />;
+  }
+
+  return (
+    <List
+      grid={{
+        gutter: 16,
+        xs: 1,
+        sm: 2,
+        md: 2,
+        lg: 3,
+        xl: 4,
+        xxl: 4,
+      }}
+      dataSource={images}
+      renderItem={(item) => (
+        <List.Item>
+          <Card
+            style={{ overflow: "hidden" }}
+            cover={
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Image
+                  src={item.content}
+                  style={{ maxWidth: 300, height: 200 }}
+                />
+              </div>
+            }
+          >
+            <Card.Meta title={item.name} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
 };
