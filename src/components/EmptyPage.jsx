31a489c1e0f0a6de77b24396b4656da49f79660d
alex-serb13 <alex.serb@globant.com>
2022-07-21 10:23:47 +0300

    add empty page component

---
+++ src/components/EmptyPage.jsx
@@ -0,0 +1,11 @@
+import { Empty, Row, Col } from "antd";
+
+export const EmptyPage = ({ description }) => {
+  return (
+    <Row type="flex" align="middle" style={{ height: "inherit" }}>
+      <Col span={12} offset={6}>
+        <Empty description={description} />
+      </Col>
+    </Row>
+  );
+};
